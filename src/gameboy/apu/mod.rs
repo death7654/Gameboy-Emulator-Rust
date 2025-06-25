bbdbd852bed7607520eb895d612987b4798dc58d
Robinson Arysseril <72635727+death7654@users.noreply.github.com>
2025-06-25 21:31:52 +0530

    start apu

---
+++ src/gameboy/apu/mod.rs
@@ -0,0 +1,22 @@
+pub struct Audio
+{
+    audio_enabled: bool,
+    channel_1: bool,
+    channel_2: bool,
+    channel_3: bool,
+    channel_4: bool,
+}
+impl Audio
+{
+    pub fn new() -> Self
+    {
+        Audio
+        {
+            audio_enabled: false,
+            channel_1: false,
+            channel_2: false,
+            channel_3: false,
+            channel_4: false,
+        }
+    }
+}
